7a40535c0a8affe60fc80811994effc2e25a5840
Keshav Sharma <Keshav.bhambi@gmail.com>
2025-08-02 13:10:12 +0530

    initial commit

--- components/CountryCard.jsx
+++ components/CountryCard.jsx
@@ -1,33 +1,35 @@
 import { Link } from "react-router";
 
-
 export default function CountryCard(props) {
-//   console.log(props);
+  //   console.log(props);
   // deconstructing country's name from the props object
-  const {name} = props;
+  const { name } = props;
   // deconstructing country's name also from the props object
-  const {flag} = props;
-  const {population} = props;
-  const {region} = props;
-  const {capital} = props;
+  const { flag } = props;
+  const { population } = props;
+  const { region } = props;
+  const { capital } = props;
   return (
     <>
-    <Link to={`/${name}`}className="country-card">
-      {/* so above here name.common will be undefined early */}
-      <img src={flag} alt="flag" />
-      <div className="card-text">
-        <h3 className="card-title">{name}</h3>
-        <p>
-          <b>Population: </b>{population.toLocaleString('en-IN')}
-        </p>
-        <p>
-          <b>Region: </b>{region}
-        </p>
-        <p>
-          <b>Capital: </b>{capital}
-        </p>
-      </div>
-    </Link>
+      <Link to={`/${name}`} className="country-card">
+        {/* so above here name.common will be undefined early */}
+        <img src={flag} alt="flag" />
+        <div className="card-text">
+          <h3 className="card-title">{name}</h3>
+          <p>
+            <b>Population: </b>
+            {population.toLocaleString("en-IN")}
+          </p>
+          <p>
+            <b>Region: </b>
+            {region}
+          </p>
+          <p>
+            <b>Capital: </b>
+            {capital}
+          </p>
+        </div>
+      </Link>
     </>
   );
 }
